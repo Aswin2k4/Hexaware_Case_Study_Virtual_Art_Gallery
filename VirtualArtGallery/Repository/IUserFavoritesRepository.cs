1f3622b0140f00e3ebb92e80285264a837efe8bf
ASWIN O <110712557+Aswin2k4@users.noreply.github.com>
2024-11-24 21:45:43 +0530

    IUserFavoritesRepository.cs

--- VirtualArtGallery/Repository/IUserFavoritesRepository.cs
+++ VirtualArtGallery/Repository/IUserFavoritesRepository.cs
@@ -1,9 +1,11 @@
-﻿namespace VirtualArtGallery.Repository
+using VirtualArtGallery.Model;
+
+namespace VirtualArtGallery.Repository
 {
     internal interface IUserFavoritesRepository
     {
         bool AddFavoriteArtwork(int userID, int artworkID);
         bool RemoveFavoriteArtwork(int userID, int artworkID);
-        List<int> GetUserFavoriteArtworks(int userID);
+        List<Artwork> GetUserFavoriteArtworks(int userID);
     }
 }
