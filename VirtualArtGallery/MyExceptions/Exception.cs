23804325c56f2ba239d31853604b258976850f76
ASWIN O <110712557+Aswin2k4@users.noreply.github.com>
2024-11-24 21:49:35 +0530

    Initial

---
+++ VirtualArtGallery/MyExceptions/Exception.cs
@@ -0,0 +1,18 @@
+﻿namespace VirtualArtGallery.Exceptions
+{
+    // Custom Exception for Artwork Not Found
+    public class ArtWorkNotFoundException : Exception
+    {
+        public ArtWorkNotFoundException(string message) : base(message)
+        {
+        }
+    }
+
+    // Custom Exception for User Not Found
+    public class UserNotFoundException : Exception
+    {
+        public UserNotFoundException(string message) : base(message)
+        {
+        }
+    }
+}
