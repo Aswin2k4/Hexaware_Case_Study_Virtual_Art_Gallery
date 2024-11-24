0184f4a36b6f4e6689d41c0faab156b44bcfaa96
ASWIN O <110712557+Aswin2k4@users.noreply.github.com>
2024-11-24 21:46:28 +0530

    IUserFavoritesService.cs

--- VirtualArtGallery/Service/IUserFavoritesService.cs
+++ VirtualArtGallery/Service/IUserFavoritesService.cs
@@ -1,9 +1,11 @@
-﻿namespace VirtualArtGallery.Service
+using VirtualArtGallery.Model;
+
+namespace VirtualArtGallery.Service
 {
     internal interface IUserFavoritesService
     {
         bool AddFavoriteArtwork(int userID, int artworkID);
         bool RemoveFavoriteArtwork(int userID, int artworkID);
-        void GetUserFavoriteArtworks(int userID);
+        List<Artwork> GetUserFavoriteArtworks(int userID);
     }
 }
